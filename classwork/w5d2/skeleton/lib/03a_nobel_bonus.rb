15ca45c21723f74765af69dce1cff5a93a983c9e
matthewgreer <matthew.greer.dev@gmail.com>
2020-06-09 18:21:15 -0400

    Tackle SQL for first time

---
+++ classwork/w5d2/skeleton/lib/03a_nobel_bonus.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+    SELECT
+      nobels.yr
+    FROM
+      nobels
+    WHERE
+      nobels.subject = 'Physics' EXCEPT (
+        SELECT
+          nobels.yr
+        FROM
+          nobels
+        WHERE
+          nobels.subject = 'Chemistry' 
+      ) 
+  SQL
+end
